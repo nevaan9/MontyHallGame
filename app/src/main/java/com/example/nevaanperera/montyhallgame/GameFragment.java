b8d00dd01d6bebffe325220b79aaf15e4cba8dd3
Nevaan Perera <nevaan9@gmail.com>
2018-09-14 21:21:00 -0400

    Wired in clickhandler functions

--- app/src/main/java/com/example/nevaanperera/montyhallgame/GameFragment.java
+++ app/src/main/java/com/example/nevaanperera/montyhallgame/GameFragment.java
@@ -7,12 +7,21 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageButton;
+import android.widget.TextView;
+
+import java.util.Random;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class GameFragment extends Fragment {
+
+    ImageButton door1 = null;
+    ImageButton door2 = null;
+    ImageButton door3 = null;
+    private boolean initialDoorPressed = false;
+    private int prize_door;
 
 
     public GameFragment() {
@@ -27,18 +36,61 @@ public class GameFragment extends Fragment {
         View rootView = inflater.inflate(R.layout.fragment_game, container, false);
 
         // Get the doors
-        ImageButton door1 = rootView.findViewById(R.id.door1);
-        ImageButton door2 = rootView.findViewById(R.id.door2);
-        ImageButton door3 = rootView.findViewById(R.id.door3);
+        door1 = rootView.findViewById(R.id.door1);
+        door2 = rootView.findViewById(R.id.door2);
+        door3 = rootView.findViewById(R.id.door3);
 
+        // Initialize the prize door
+        prize_door = (int) (Math.random() * 3 + 1);
+        System.out.println(prize_door);
+
+        // Get the header
+        final TextView game_activity_heaer = rootView.findViewById(R.id.game_activity_header);
+
+        // Set onClick Listerner for door1
         door1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Hello! I just got clicke!");
+                if (!initialDoorPressed) {
+                    door1.setImageLevel(1);
+                    doorPressed();
+                    initialDoorPressed = true;
+                    System.out.println(prize_door);
+                }
+            }
+        });
+
+        // Set onClick Listerner for door2
+        door2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!initialDoorPressed){
+                    door2.setImageLevel(1);
+                    doorPressed();
+                    initialDoorPressed = true;
+                    System.out.println(prize_door);
+                }
+            }
+        });
+
+        // Set onClick Listerner for door1
+        door3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (!initialDoorPressed) {
+                    door3.setImageLevel(1);
+                    doorPressed();
+                    initialDoorPressed = true;
+                    System.out.println(prize_door);
+                }
             }
         });
 
         return rootView;
+    }
+
+    public void doorPressed () {
+        // Do something
     }
 
 }
